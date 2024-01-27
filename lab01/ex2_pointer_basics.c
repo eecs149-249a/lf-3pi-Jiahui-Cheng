d8e108d0d7ffe99f85445e5f985a686bf36cc88a
Echo Cheng <chengjiahui@Echos-MacBook-Air.local>
2024-01-27 12:01:16 +0800

    Lab1 finished

--- lab01/ex2_pointer_basics.c
+++ lab01/ex2_pointer_basics.c
@@ -3,14 +3,16 @@
 int main() {
   // Assign x (an integer) to 5
   int x = 5;
-
+ 
   // TODO: create a pointer to x
   // Hint: the first blank should be a variable type
   //       the second blank should be the address of x
-  ______ pointer_to_x = ________;
+  int* pointer_to_x = &x;
 
   // This line should print 5
-  printf("%d\n", *pointer_to_x);
+  printf("Dereference pointer to x:%d\n", *pointer_to_x);
+  printf("Access content of pointer to x:%p\n",pointer_to_x);
+  printf("Access address of x:%p\n",&x);
 
   return 0;
 }
